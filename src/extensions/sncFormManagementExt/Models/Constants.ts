e2dd0765b91050e564a8e7c119fa17d743efa67a
roey-deeplan <150326749+roey-deeplan@users.noreply.github.com>
2024-08-05 15:23:55 +0300

    updated BusinessTravel and MeetingRoom

---
+++ src/extensions/sncFormManagementExt/Models/Constants.ts
@@ -0,0 +1,11 @@
+export class Constants {
+  // Business Travel
+  public static readonly newBusinessTravelRequest: string = "/SitePages/newBusinessTravelRequest.aspx";
+  public static readonly editBusinessTravelRequest: string = "/SitePages/editBusinessTravelRequest.aspx";
+  public static readonly BUSINESS_TRAVEL_LIBRARY_NAME: Array<string> = ["BusinessTravelRequest"];
+
+  //Meeting Room
+  public static readonly newMeetingRoomRequest: string = "/SitePages/newMeetingRoomRequest.aspx";
+  public static readonly editMeetingRoomRequest: string = "/SitePages/editMeetingRoomRequest.aspx";
+  public static readonly MEETING_ROOM_LIBRARY_NAME: Array<string> = ["MeetingRoomRequest"];
+}
--- src/extensions/sncFormManagementExt/SncFormManagementExtCommandSet.ts
+++ src/extensions/sncFormManagementExt/SncFormManagementExtCommandSet.ts
@@ -1,11 +1,13 @@
 import { Log } from '@microsoft/sp-core-library';
 import {
   BaseListViewCommandSet,
+  IListViewCommandSetListViewUpdatedParameters,
   type Command,
   type IListViewCommandSetExecuteEventParameters,
-  type ListViewStateChangedEventArgs
+  // type ListViewStateChangedEventArgs
 } from '@microsoft/sp-listview-extensibility';
-import { Dialog } from '@microsoft/sp-dialog';
+import { override } from "@microsoft/decorators";
+import { Constants } from "./Models/Constants";
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -14,8 +16,11 @@ import { Dialog } from '@microsoft/sp-dialog';
  */
 export interface ISncFormManagementExtCommandSetProperties {
   // This is an example; replace with your own properties
-  sampleTextOne: string;
-  sampleTextTwo: string;
+  New_BusinessTravel_Field: string;
+  Edit_BusinessTravel_Field: string;
+
+  New_MeetingRoom_Field: string;
+  Edit_MeetingRoom_Field: string;
 }
 
 const LOG_SOURCE: string = 'SncFormManagementExtCommandSet';
@@ -24,45 +29,82 @@ export default class SncFormManagementExtCommandSet extends BaseListViewCommandS
 
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, 'Initialized SncFormManagementExtCommandSet');
-
-    // initial state of the command's visibility
-    const compareOneCommand: Command = this.tryGetCommand('COMMAND_1');
-    compareOneCommand.visible = false;
-
-    this.context.listView.listViewStateChangedEvent.add(this, this._onListViewStateChanged);
-
     return Promise.resolve();
   }
 
+  public onListViewUpdated(event: IListViewCommandSetListViewUpdatedParameters): void {
+    //* Create and Update Vacation Request
+    const BusinessTravelCompareOneCommand: Command = this.tryGetCommand(
+      "New_BusinessTravel_Field"
+    );
+    const BusinessTravelSecondCommand: Command = this.tryGetCommand(
+      "Edit_BusinessTravel_Field"
+    );
+
+    //* Create and Update ITCalls Request
+    const MeetingRoomCompareOneCommand: Command = this.tryGetCommand(
+      "New_MeetingRoom_Field"
+    );
+    const MeetingRoomCompareSecondCommand: Command = this.tryGetCommand(
+      "Edit_MeetingRoom_Field"
+    );
+
+    //* Get current library name from pageContext
+    let LibraryName = this.context.pageContext.list?.title;
+
+    //* Check if current library name is in Constants.BUSINESS_TRAVEL_LIBRARY_NAME
+    if (Constants.BUSINESS_TRAVEL_LIBRARY_NAME.indexOf(LibraryName || "") !== -1) {
+      BusinessTravelCompareOneCommand.visible = true;
+      if (BusinessTravelSecondCommand) {
+        BusinessTravelSecondCommand.visible = event.selectedRows?.length === 1;
+      }
+      require("./ExtStyle/ExtStyle.css");
+    } else {
+      BusinessTravelCompareOneCommand.visible = false;
+      BusinessTravelSecondCommand.visible = false;
+    }
+
+    //* Check if current library name is in Constants.MEETING_ROOM_LIBRARY_NAME
+    if (Constants.MEETING_ROOM_LIBRARY_NAME.indexOf(LibraryName || "") !== -1) {
+      MeetingRoomCompareOneCommand.visible = true;
+      if (MeetingRoomCompareSecondCommand) {
+        MeetingRoomCompareSecondCommand.visible = event.selectedRows?.length === 1;
+      }
+      require("./ExtStyle/ExtStyle.css");
+    } else {
+      MeetingRoomCompareOneCommand.visible = false;
+      MeetingRoomCompareSecondCommand.visible = false;
+    }
+
+  }
+
+  @override
   public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
+    const webUri = this.context.pageContext.web.absoluteUrl;
+
     switch (event.itemId) {
-      case 'COMMAND_1':
-        Dialog.alert(`${this.properties.sampleTextOne}`).catch(() => {
-          /* handle error */
-        });
+      //* Create and Update BusinessTravel Request
+      case "New_BusinessTravel_Field":
+        window.location.href = webUri + Constants.newBusinessTravelRequest;
         break;
-      case 'COMMAND_2':
-        Dialog.alert(`${this.properties.sampleTextTwo}`).catch(() => {
-          /* handle error */
-        });
+      case "Edit_BusinessTravel_Field":
+        var ItemID = event.selectedRows[0].getValueByName("ID").toString();
+        window.location.href =
+          webUri + Constants.editBusinessTravelRequest + "?FormID=" + ItemID;
         break;
+
+      //* Create and Update MeetingRoom Request
+      case "New_MeetingRoom_Field":
+        window.location.href = webUri + Constants.newMeetingRoomRequest;
+        break;
+      case "Edit_MeetingRoom_Field":
+        var ItemID = event.selectedRows[0].getValueByName("ID").toString();
+        window.location.href =
+          webUri + Constants.editMeetingRoomRequest + "?FormID=" + ItemID;
+        break;
+
       default:
         throw new Error('Unknown command');
     }
-  }
-
-  private _onListViewStateChanged = (args: ListViewStateChangedEventArgs): void => {
-    Log.info(LOG_SOURCE, 'List view state changed');
-
-    const compareOneCommand: Command = this.tryGetCommand('COMMAND_1');
-    if (compareOneCommand) {
-      // This command should be hidden unless exactly one row is selected.
-      compareOneCommand.visible = this.context.listView.selectedRows?.length === 1;
-    }
-
-    // TODO: Add your logic here
-
-    // You should call this.raiseOnChage() to update the command bar
-    this.raiseOnChange();
   }
 }
