f28467629ed8f0691257f3597f6de9ca3de62d32
roey-deeplan <150326749+roey-deeplan@users.noreply.github.com>
2024-08-20 17:11:17 +0300

    Changing library name to ["My Travel Requests"]

--- src/extensions/sncFormManagementExt/Models/Constants.ts
+++ src/extensions/sncFormManagementExt/Models/Constants.ts
@@ -2,7 +2,7 @@ export class Constants {
   // Business Travel
   public static readonly newBusinessTravelRequest: string = "/SitePages/Business-travel-request-new.aspx";
   public static readonly editBusinessTravelRequest: string = "/SitePages/Business-Travel-Request-Edit.aspx";
-  public static readonly BUSINESS_TRAVEL_LIBRARY_NAME: Array<string> = ["Business Travel Request"];
+  public static readonly BUSINESS_TRAVEL_LIBRARY_NAME: Array<string> = ["My Travel Requests"];
 
   //Meeting Room
   public static readonly newMeetingRoomRequest: string = "/SitePages/newMeetingRoomRequest.aspx";
